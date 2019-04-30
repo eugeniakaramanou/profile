7c33b7b5e9a72f70c81d923025605856ff3fa15f
Eugenia Karamanou <eugeniakaramanou@sysadmins-MacBook-Pro.local>
2019-04-30 16:37:53 +0200

    simple html page with some css anim
    Committer: Eugenia Karamanou <eugeniakaramanou@gmail.com>

---
+++ gulpfile.js
@@ -0,0 +1,78 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+var useref = require('gulp-useref');
+var uglify = require('gulp-uglify');
+var gulpIf = require('gulp-if');
+var cssnano = require('gulp-cssnano');
+var imagemin = require('gulp-imagemin');
+var del = require('del');
+var runSequence = require('run-sequence');
+
+gulp.task('sass', function() {
+    return gulp.src('app/scss/**/*.scss') // Gets all files ending with .scss in app/scss
+        .pipe(sass())
+        .pipe(gulp.dest('app/css'))
+        .pipe(browserSync.reload({
+            stream: true
+        }))
+});
+
+// Start browserSync server
+gulp.task('browserSync', function() {
+    browserSync.init({
+        server: {
+            baseDir: 'app'
+        }
+    });
+});
+
+// Watchers
+gulp.task('watch', ['browserSync', 'sass'], function() {
+    gulp.watch('app/scss/**/*.scss', ['sass']);
+    gulp.watch('app/*.html', browserSync.reload);
+    gulp.watch('app/js/**/*.js', browserSync.reload);
+});
+
+// Optimizing CSS and JavaScript
+gulp.task('useref', function(){
+    return gulp.src('app/*.html')
+        .pipe(useref())
+        // Minifies only if it's a JavaScript file
+        .pipe(gulpIf('*.js', uglify()))
+        // Minifies only if it's a CSS file
+        .pipe(gulpIf('*.css', cssnano()))
+        .pipe(gulp.dest('dist'))
+});
+
+// Optimizing Images
+gulp.task('images', function(){
+    return gulp.src('app/assets/images/**/*.+(png|jpg|gif|svg)')
+        .pipe(imagemin())
+        .pipe(gulp.dest('dist/assets/images'))
+});
+
+// Copying fonts
+gulp.task('fonts', function() {
+    return gulp.src('app/assets/fonts/**/*')
+        .pipe(gulp.dest('dist/assets/fonts'))
+});
+
+// Cleaning
+gulp.task('clean:dist', function() {
+    return del.sync('dist');
+});
+
+// Build Sequences
+gulp.task('build', function (callback) {
+    runSequence('clean:dist',
+        ['sass', 'useref', 'images', 'fonts'],
+        callback
+    )
+});
+
+gulp.task('default', function (callback) {
+    runSequence(['sass','browserSync', 'watch'],
+        callback
+    )
+});
